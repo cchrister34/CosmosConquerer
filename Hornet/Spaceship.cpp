4c51f783956776c99ceca23a5c435e708ceabaa8
cchrister34 <w23022158@northumbria.ac.uk>
2025-03-04 22:41:29 +0000

    Added the ability to turn left and right using A & D keys as well as locking the camera to the ships position

--- Hornet/Spaceship.cpp
+++ Hornet/Spaceship.cpp
@@ -1,7 +1,22 @@
 #include "Spaceship.h"
 #include "HtKeyboard.h"
+#include "HtCamera.h"
 #include "Bullet.h"
 #include "ObjectManager.h"
+
+//
+const double THRUST = 230.0;
+const int ROTATIONLEFT = -90;
+const int ROTATIONRIGHT = 90;
+const double SHOOT_DELAY = 0.3;
+const double SHIP_SIZE = 1.25;
+const double FRICTION_STRENGTH = 12.0;
+const double BULLET_MAGNITUDE = 60;
+const double BULLET_SPEED = 800.0;
+const std::string SHIP_IMAGE = "assets/spaceship.png"; //Cannot use const char* because of one definition rule 
+const std::string ENGINE_SOUND = "assets/thrustloop.wav";
+const std::string BULLET_SOUND = "assets/zap.wav";
+const std::string EXPLOSION_SOUND = "assets/explosion1.wav";
 
 Spaceship::Spaceship(ObjectType objType) : GameObject(objType)
 {
@@ -11,6 +26,8 @@ void Spaceship::Update(double frametime)
 {
     m_position = m_position + m_velocity * frametime;
     m_shootdelay -= frametime;
+
+    HtCamera::instance.PlaceAt(Vector2D(0, m_position.YValue));
 
     if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_W))
     {
@@ -34,6 +51,17 @@ void Spaceship::Update(double frametime)
         HtAudio::instance.Stop(m_engineSoundChannel);
         isEnginePlaying = false;
     }
+
+    if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_A))
+    {
+        m_angle = m_angle + ROTATIONLEFT * frametime;
+    }
+
+    if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_D))
+    {
+        m_angle = m_angle + ROTATIONRIGHT * frametime;
+    }
+
 
     if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_SPACE) && m_shootdelay < 0)
     {
@@ -71,15 +99,16 @@ void Spaceship::ProcessCollision(GameObject& other)
 
 void Spaceship::Initialise()
 {
-    m_position.set(0,-900);
+    m_position.set(-1600,0);
     m_velocity.set(0, 0);
     LoadImage(SHIP_IMAGE.c_str()); //c_str used to convert sting to const char
     m_scale = SHIP_SIZE; 
+    m_angle = 90;
     m_engineSound = HtAudio::instance.LoadSound(ENGINE_SOUND.c_str());
 
     m_shootdelay = SHOOT_DELAY;
-    m_bulletSound = HtAudio::instance.LoadSound(BULLET_SOUND);
-    m_explosionBang = HtAudio::instance.LoadSound(EXPLOSION_SOUND);
+    m_bulletSound = HtAudio::instance.LoadSound(BULLET_SOUND.c_str());
+    m_explosionBang = HtAudio::instance.LoadSound(EXPLOSION_SOUND.c_str());
 
     SetCollidable();
 }
--- Hornet/Spaceship.h
+++ Hornet/Spaceship.h
@@ -25,15 +25,4 @@ private:
     Vector2D m_bulletPosition;
     Vector2D m_bulletSpeed;
 
-    //Player Constants because there is only one player
-    const double THRUST = 230.0;
-    const double SHOOT_DELAY = 0.3;
-    const double SHIP_SIZE = 1.25;
-    const double FRICTION_STRENGTH = 2.0;
-    const double BULLET_MAGNITUDE = 60;
-    const double BULLET_SPEED = 800.0;
-    const std::string SHIP_IMAGE = "assets/spaceship.png"; //Cannot use const char* because of one definition rule 
-    const std::string ENGINE_SOUND = "assets/thrustloop.wav";
-    const std::string BULLET_SOUND = "assets/zap.wav";
-    const std::string EXPLOSION_SOUND = "assets/explosion1.wav";
 };
